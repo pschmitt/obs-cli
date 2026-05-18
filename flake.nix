1fadddf6513087eaa75fe941f302d92e2865013c
Philipp Schmitt <philipp@schmitt.co>
2026-05-18 10:45:51 +0200

    chore: bump version to 0.9.5

--- flake.nix
+++ flake.nix
@@ -51,7 +51,7 @@
 
         obsCli = pyPkgs.buildPythonApplication {
           pname = "obs-cli";
-          version = "0.9.4";
+          version = "0.9.5";
           src = ./.;
           pyproject = true;
           nativeBuildInputs = with pyPkgs; [
